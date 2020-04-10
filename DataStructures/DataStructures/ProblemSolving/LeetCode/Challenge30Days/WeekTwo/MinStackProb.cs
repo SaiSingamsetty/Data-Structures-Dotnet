1e050fef8ffa67dd96e18c97a67f30e66af602e4
Sai Singamsetty <Sai.Singamsetty@Honeywell.com>
2020-04-10 14:58:34 +0530

    Challenge10 MinStack Solved

---
+++ DataStructures/DataStructures/ProblemSolving/LeetCode/Challenge30Days/WeekTwo/MinStackProb.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using models = DataStructures.ProblemSolving.LeetCode.Challenge30Days.WeekTwo.Models;
+
+namespace DataStructures.ProblemSolving.LeetCode.Challenge30Days.WeekTwo
+{
+    //LEETCODE Challenge10: https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3292/
+    // Design a stack that supports push, pop, top, and retrieving the minimum element in constant time. 
+    // push(x) -- Push element x onto stack.
+    // pop() -- Removes the element on top of the stack.
+    // top() -- Get the top element.
+    // getMin() -- Retrieve the minimum element in the stack.
+
+
+    public class MinStackProb
+    {
+        public static void Init()
+        {
+            SolveUsingMinValueStack();
+            SolveUsingStackObject();
+        }
+
+        private static void SolveUsingMinValueStack()
+        {
+            // This MinStack is designed to use extra minimum value stack
+            var ms = new models.MinStack();
+            ms.Pop();
+            ms.Top();
+            ms.GetMin();
+
+            ms.Push(0);
+            ms.Push(-1);
+            ms.Push(-2);
+            ms.Push(3);
+            ms.GetMin();
+            ms.Top();
+        }
+
+        private static void SolveUsingStackObject()
+        {
+            var ms = new MinStack();
+            ms.Push(0);
+            ms.Push(-1);
+            ms.Pop();
+            ms.Push(-2);
+            ms.Push(3);
+            ms.GetMin();
+            ms.Top();
+        }
+
+    }
+
+    #region HelperClasses
+
+    public class MinStack
+    {
+        private StackObject _topStackObject;
+
+        public void Push(int x)
+        {
+            if (_topStackObject == null)
+            {
+                _topStackObject = new StackObject(x);
+            }
+            else
+            {
+                var temp = new StackObject(x)
+                {
+                    Next = _topStackObject,
+                    MinValue = Math.Min(x, _topStackObject.MinValue)
+                };
+                _topStackObject = temp;
+            }
+        }
+
+        public void Pop()
+        {
+            if (_topStackObject == null)
+            {
+                return;
+            }
+
+            _topStackObject = _topStackObject.Next;
+        }
+
+        public int Top()
+        {
+            if (_topStackObject == null)
+            {
+                return int.MaxValue;
+            }
+
+            return _topStackObject.Value;
+        }
+
+        public int GetMin()
+        {
+            if (_topStackObject == null)
+            {
+                return int.MaxValue;
+            }
+
+            return _topStackObject.MinValue;
+        }
+    }
+
+    public class StackObject
+    {
+        public int Value;
+
+        public int MinValue;
+
+        public StackObject Next;
+
+        public StackObject(int value)
+        {
+            Value = value;
+            MinValue = value;
+            Next = null;
+        }
+    }
+
+    #endregion
+}
---
+++ DataStructures/DataStructures/ProblemSolving/LeetCode/Challenge30Days/WeekTwo/Models/MinStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.ProblemSolving.LeetCode.Challenge30Days.WeekTwo.Models
+{
+    public class MinStack
+    {
+        /** initialize your data structure here. */
+        private readonly Stack<int> _stack;
+        private readonly Stack<int> _minValueStack;
+
+        public MinStack()
+        {
+            _stack = new Stack<int>();
+            _minValueStack = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            if (_minValueStack.Count == 0 || x <= _minValueStack.Peek())
+                _minValueStack.Push(x);
+            _stack.Push(x);
+        }
+
+        public void Pop()
+        {
+            if(_stack.Count == 0 || _minValueStack.Count ==0)
+                return;
+            
+            if (_stack.Peek().Equals(_minValueStack.Peek()))
+            {
+                _minValueStack.Pop();
+            }
+            _stack.Pop();
+        }
+
+        public int Top()
+        {
+            if (_stack.Count == 0)
+                return int.MaxValue;
+
+            return _stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            if (_minValueStack.Count == 0)
+                return int.MaxValue;
+
+            return _minValueStack.Peek();
+        }
+    }
+}
