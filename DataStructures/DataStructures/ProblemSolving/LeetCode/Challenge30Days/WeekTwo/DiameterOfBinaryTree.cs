819153fccd381d47e0b5b9e6da89283f26c434f1
Sai Singamsetty <Sai.Singamsetty@Honeywell.com>
2020-04-12 23:48:52 +0530

    DiameterOfBinaryTree Revisited

--- DataStructures/DataStructures/ProblemSolving/LeetCode/Challenge30Days/WeekTwo/DiameterOfBinaryTree.cs
+++ DataStructures/DataStructures/ProblemSolving/LeetCode/Challenge30Days/WeekTwo/DiameterOfBinaryTree.cs
@@ -3,75 +3,52 @@ using DataStructures.ProblemSolving.LeetCode.Challenge30Days.WeekTwo.Models;
 
 namespace DataStructures.ProblemSolving.LeetCode.Challenge30Days.WeekTwo
 {
-    //Challenge11: https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3293/
-    //Given a binary tree, you need to compute the length of the diameter of the tree. The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
+    //Challenge11 https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3293/
+    //Given a binary tree, you need to compute the length of the diameter of the tree.
+    //The diameter of a binary tree is the length of the longest path between any two nodes in a tree.
     //This path may or may not pass through the root.
-    //Note: The length of path between two nodes is represented by the number of edges between them.
 
     public class DiameterOfBinaryTree
     {
         public static void Init()
         {
-            TreeNode rootNode = new TreeNode(1);
-            rootNode.left = new TreeNode(2);
-            rootNode.right = new TreeNode(3);
+            var rootNode = new TreeNode(1)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(3)
+            };
             rootNode.left.left = new TreeNode(4);
             rootNode.left.right = new TreeNode(5);
-            var response = GetDiameter(rootNode);
+
+            var response = FindDiameterOfBinaryTree(rootNode);
+            Console.WriteLine(response);
         }
 
-        private static int GetDiameter(TreeNode root)
-        {
-            var response = Diameter(root);
-            return response;
-        }
+        #region Approach 1
 
-        private static int DiameterOpt(TreeNode root, Height height)
+        private static int FindDiameterOfBinaryTree(TreeNode root)
         {
-            /* lh --> Height of left subtree 
-               rh --> Height of right subtree */
-            Height lh = new Height(), rh = new Height();
-
             if (root == null)
-            {
-                height.h = 0;
-                return 0; /* diameter is also 0 */
-            }
+                return 0;
 
-            /* ldiameter  --> diameter of left subtree 
-               rdiameter  --> Diameter of right subtree */
-            /* Get the heights of left and right subtrees in lh and rh 
-             And store the returned values in ldiameter and ldiameter */
-            int ldiameter = DiameterOpt(root.left, lh);
-            int rdiameter = DiameterOpt(root.right, rh);
-
-            /* Height of current node is max of heights of left and 
-             right subtrees plus 1*/
-            height.h = Math.Max(lh.h, rh.h) + 1;
-
-            return Math.Max(lh.h + rh.h + 1, Math.Max(ldiameter, rdiameter));
+            var maxDiameter = 0; // We can also use global/static variable
+            FindMaxDepth(root, ref maxDiameter);
+            return maxDiameter;
         }
 
-        static int Diameter(TreeNode root)
+        private static int FindMaxDepth(TreeNode node, ref int maxDiameter)
         {
-            Height height = new Height();
-            return DiameterOpt(root, height);
-        }
-
-        static int Height(TreeNode node)
-        {
-            /* base case tree is empty */
             if (node == null)
                 return 0;
 
-            /* If tree is not empty then height = 1 + max of left 
-               height and right heights */
-            return (1 + Math.Max(Height(node.left), Height(node.right)));
-        }
-    }
+            var leftTreeDepth = FindMaxDepth(node.left, ref maxDiameter);
+            var rightTreeDepth = FindMaxDepth(node.right, ref maxDiameter);
 
-    public class Height
-    {
-        public int h;
+            maxDiameter = Math.Max(maxDiameter, leftTreeDepth + rightTreeDepth);
+
+            return 1 + Math.Max(leftTreeDepth, rightTreeDepth);
+        }
+
+        #endregion
     }
 }
