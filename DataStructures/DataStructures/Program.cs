6339fff470e28b9a47956de4f75d81a980bf0f7c
Sai Singamsetty <Sai.Singamsetty@Honeywell.com>
2020-05-09 18:37:45 +0530

    May-Week2-Day2 PerfectSquare Solved

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DataStructures.ProblemSolving.LeetCode.Challenge30Days.May.WeekOne;
 using DataStructures.ProblemSolving.LeetCode.Problems;
 using System;
+using DataStructures.ProblemSolving.LeetCode.Challenge30Days.May.WeekTwo;
 
 namespace DataStructures
 {
@@ -8,20 +9,24 @@ namespace DataStructures
     {
         private static void Main()
         {
+            //TODAY'S LEETCODE: NOT YET
+            PerfectSquare.Init();
+
             //Refactor
             ImplementStrStrLc28.Init();
 
-            Todo();
-
             //Refactor
             NumberComplement.Init();
-            
+
+            Todo();
+
             Console.ReadKey();
         }
 
         private static void Todo()
         {
-            //TODO:https://leetcode.com/problems/daily-temperatures/
+            //TODO: Lending Cart in Whats-app group
+            //TODO: https://leetcode.com/problems/daily-temperatures/
 
             //TODO: https://www.youtube.com/watch?v=oheO0YejMd8&feature=youtu.be
             //TODO: https://www.youtube.com/watch?v=5o-kdjv7FD0
