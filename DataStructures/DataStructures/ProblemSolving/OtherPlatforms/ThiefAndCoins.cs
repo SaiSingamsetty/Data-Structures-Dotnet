e04077231f4c38d1385bcc572947c50b8ade1b34
Sai Singamsetty <saitejas191@gmail.com>
2021-04-14 21:41:16 +0530

    ThiefAndCoin optimized space

--- DataStructures/DataStructures/ProblemSolving/OtherPlatforms/ThiefAndCoins.cs
+++ DataStructures/DataStructures/ProblemSolving/OtherPlatforms/ThiefAndCoins.cs
@@ -9,7 +9,7 @@ namespace DataStructures.ProblemSolving.OtherPlatforms
         public static void Execute()
         {
             var tc1 = new int[3][];
-            tc1[0] = new[] {1, 3, 3};
+            tc1[0] = new[] {1, 9, 3};
             tc1[1] = new[] {2, 1, 4};
             tc1[2] = new[] {0, 6, 4};
 
@@ -22,8 +22,7 @@ namespace DataStructures.ProblemSolving.OtherPlatforms
             var cols = arr[0].Length;
             var maxSoFar = int.MinValue;
 
-            var temp = new int[rows, cols];
-
+            
             for (var i = 0; i < cols; i++)
             {
 
@@ -32,25 +31,24 @@ namespace DataStructures.ProblemSolving.OtherPlatforms
 
                     if (i == 0)
                     {
-                        temp[j, i] = arr[j][i];
                         continue;
                     }
 
                     if (j == 0)
                     {
-                        temp[j, i] = arr[j][i] + Math.Max(temp[j, i - 1], temp[j + 1, i - 1]);
+                        arr[j][i] = arr[j][i] + Math.Max(arr[j][i - 1], arr[j + 1][i - 1]);
                     }
                     else if (j == rows - 1)
                     {
-                        temp[j, i] = arr[j][i] + Math.Max(temp[j - 1, i - 1], temp[j, i - 1]);
+                        arr[j][i] = arr[j][i] + Math.Max(arr[j - 1][i - 1], arr[j][i - 1]);
                     }
                     else
                     {
-                        temp[j, i] = arr[j][i] + Math.Max(temp[j - 1, i - 1], Math.Max(temp[j, i - 1], temp[j + 1, i - 1]));
+                        arr[j][i] = arr[j][i] + Math.Max(arr[j - 1][i - 1], Math.Max(arr[j][i - 1], arr[j + 1][i - 1]));
                     }
 
-                    if (temp[j, i] > maxSoFar)
-                        maxSoFar = temp[j, i];
+                    if (arr[j][i] > maxSoFar)
+                        maxSoFar = arr[j][i];
                 }
 
             }
